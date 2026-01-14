b75ab4a1c31174a50162bdad9289cc8c0d4efc40
maliszewskiadrian <maliszewskiadrian01@gmail.com>
2026-01-14 23:32:13 +0100

    Update trigger.rs

--- src/ett/trigger.rs
+++ src/ett/trigger.rs
@@ -1,13 +1,17 @@
 use crate::esal_core::classification::KnowledgeClass;
-pub use super::state::ETTState;
+use super::state::ETTState;
 use super::reason::ETTReason;
 
+/// Deterministic ETT trigger.
+///
+/// FAIL-CLOSED:
+/// any non-grounded knowledge results in HALT.
 pub fn ett_trigger(
     class: KnowledgeClass,
 ) -> Result<ETTState, ETTReason> {
     match class {
         KnowledgeClass::Grounded => Ok(ETTState::Allow),
         KnowledgeClass::Ungrounded => Err(ETTReason::Ungrounded),
-        KnowledgeClass::Contradiction => Err(ETTReason::Contradiction),
+        KnowledgeClass::Contradictory => Err(ETTReason::Contradiction),
     }
 }
